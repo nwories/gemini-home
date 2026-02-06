a978401cf892b147f2e356b48c683a4ab1a798db
nwories <nwories>
2026-02-06 09:47:22 +0100

    overflow still not working fine

--- js/index.js
+++ js/index.js
@@ -1,22 +1,24 @@
-const menu = document.querySelector('.mobile-menu');
-const overlay = document.querySelector('.overlay');
-const menuOpen = document.querySelector('.menu-btn');
-const menuClose = document.querySelector('.close');
+document.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.mobile-menu');
+  const overlay = document.querySelector('.overlay');
+  const menuOpen = document.querySelector('.menu-btn');
+  const menuClose = document.querySelector('.close');
 
-const openMenu = () => {
-  menu.style.transform = 'translateX(0)';
-  menuClose.style.display = 'block';
-  overlay.style.display = 'block';
-  document.body.style.overflow = 'hidden';
-};
+  const openMenu = () => {
+    menu.style.transform = 'translateX(0)';
+    menuClose.style.display = 'block';
+    overlay.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+  };
 
-const closeMenu = () => {
-  menu.style.transform = 'translateX(110%)';
-  menuClose.style.display = 'none';
-  overlay.style.display = 'none';
-  document.body.style.overflow = '';
-};
+  const closeMenu = () => {
+    menu.style.transform = 'translateX(110%)';
+    menuClose.style.display = 'none';
+    overlay.style.display = 'none';
+    document.body.style.overflow = '';
+  };
 
-menuOpen.addEventListener('click', openMenu);
-menuClose.addEventListener('click', closeMenu);
-overlay.addEventListener('click', closeMenu);
+  menuOpen.addEventListener('click', openMenu);
+  menuClose.addEventListener('click', closeMenu);
+  overlay.addEventListener('click', closeMenu);
+});
